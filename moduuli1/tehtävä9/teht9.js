6e59e3edf74c67d6aa3da84ac9ea68c4ca2a9d07
Aleksanteri <aleksanteri.laitinen@metropolia.fi>
2024-11-04 21:18:16 +0200

    neliöjuuri prime tarkistus

--- moduuli1/tehtävä9/teht9.js
+++ moduuli1/tehtävä9/teht9.js
@@ -3,7 +3,7 @@
 //Palauttaa True jos prime, false jos ei
 function isPrime(num) {
   if (num === 0 || num === 1) return false;
-  for (let i = 2; i < num; i++) {
+  for (let i = 2; i < Math.floor(num**0.5); i++) {
     if (num % i === 0) {
       return false;
     }
