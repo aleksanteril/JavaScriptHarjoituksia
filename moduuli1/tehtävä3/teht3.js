a2a1d6ea5f2a686be91855e2785cb9febfcb293a
Aleksanteri Laitinen <aleksanteri.laitinen@metropolia.fi>
2024-11-04 13:02:48 +0200

    tehtävien tekoa

---
+++ moduuli1/tehtävä3/teht3.js
@@ -0,0 +1,16 @@
+'use strict';
+
+//Kysytään käyttäjältä 3 eri lukua 3 promptilla
+const input1 = parseInt(prompt('Anna 1. kokonaisluku'));
+const input2 = parseInt(prompt('Anna 2. kokonaisluku'));
+const input3 = parseInt(prompt('Anna 3. kokonaisluku'));
+
+//Tehdään laskutoimituksia. summa, tulo ja keskiarvo
+const sum = input1 + input2 + input3;
+const product = input1 * input2 * input3;
+const avrg = sum / 3;
+
+//Lisätään dokumenttiin homma
+document.querySelector('#sum').innerHTML = 'Summa on: ' + sum;
+document.querySelector('#product').innerHTML = 'Tulo on: ' + product;
+document.querySelector('#average').innerHTML = 'Keskiarvo on: ' + avrg;
---
+++ moduuli1/tehtävä4/teht4.js
@@ -0,0 +1,32 @@
+'use strict';
+
+//Funktio ottaa min ja max luvut, ja kaavalla math.random tekee float luvun välillä 0-1
+// se kerrottaan maximilla ja plussataan minimi jotta ei mennä alle sen
+function getRandomInt(minInt, maxInt) {
+  return Math.floor(Math.random() * maxInt + minInt);
+}
+
+
+//Kysytään käyttäjän nimi ja asetetaan house muuttuja
+const name = prompt('Hattu kysyy nimeäsi. Syötä nimi.');
+var house = ''
+
+//Valinta rakenne killan arvontaa varten
+switch (getRandomInt(1,4)) {
+  case 1:
+    house = 'Ravenclaw';
+    break;
+  case 2:
+    house = 'Griffindor';
+    break;
+  case 3:
+    house = 'Slytherin'
+    break;
+  case 4:
+    house = 'Hufflepuff'
+    break;
+}
+
+
+//Lisätään tulokset html fileen
+document.querySelector('#decision').innerHTML = name + ', you are ' + house;
