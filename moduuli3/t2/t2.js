d4371ca3ff3cb3697d08780c98a578066d67318b
Aleksanteri <aleksanteri.laitinen@metropolia.fi>
2024-11-11 19:18:08 +0200

    tehtävien tekoa

--- moduuli3/t2/t2.js
+++ moduuli3/t2/t2.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const ulList = document.querySelector('#target');
-let textArray = ['First item', 'Second item', 'Third item'];
+const list = ['First item', 'Second item', 'Third item'];
 
 
 for (let i = 0; i < 3; i++) {
     let listItem = document.createElement('li');
-    let text = document.createTextNode(textArray[i]);
+    let text = document.createTextNode(list[i]);
     listItem.appendChild(text);
     ulList.appendChild(listItem);
 }
