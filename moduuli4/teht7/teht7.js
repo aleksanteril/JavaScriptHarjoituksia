06a05c8683df3b25353577e40da808032c74b516
Aleksanteri Laitinen <aleksanteri.laitinen@metropolia.fi>
2024-11-18 13:11:53 +0200

    tehtävien tekoa

--- moduuli4/teht7/teht7.js
+++ moduuli4/teht7/teht7.js
@@ -1,9 +1,21 @@
 'use strict';
 
+
+function decodePolylineAddToMap(routeData) {
+    console.log(routeData);
+    for (let route of routeData) {
+        const mapPoints = L.Polyline.fromEncoded(route.legGeometry.points).getLatLngs();
+        L.polyline(mapPoints).setStyle('blue').addTo(map);
+    }
+    const timeWastedUNIX = +routeData[routeData.length-1].endTime - +routeData[0].startTime
+    routeTime.innerHTML = 'Time required '+ (timeWastedUNIX/1000/60).toFixed(0) +' minutes' //Time shown in minutes
+}
+
+
 //Karaportti 2 =  lat 60.223876 lon 24.758061
 //Funktiolla haetaan koordinaattien perusteella tiedot reitistä karaportti 2 paikkaan, palautetaan tiedot
 async function queryRoute(coordinates) {
-    const param = {"query": `{plan(from: {lat: ${coordinates[1]}, lon: ${coordinates[0]}}to: {lat: 60.223876, lon: 24.758061}numItineraries: 1) {itineraries {legs {startTime endTime mode duration realTime distance transitLeg legGeometry{points length}}}}}`}
+    const param = {"query": `{plan(from: {lat: ${coordinates[1]}, lon: ${coordinates[0]}}to: {lat: 60.223876, lon: 24.758061}numItineraries: 1) {itineraries {legs {startTime endTime mode duration realTime distance transitLeg legGeometry{points}}}}}`}
     const response = await fetch(urlRoute, {
         method: 'POST',
         body: JSON.stringify(param),
@@ -23,8 +35,16 @@ async function queryCoordinates() {
     return data.features[0].geometry.coordinates;
 }
 
+//Luodaan kartta johon laitetaan reitit
+const map = L.map('map').setView([60.223876, 24.758061], 11);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
 
 //Tuodaan elementit ja luodaan vakio-arvoja
+const routeTime = document.querySelector('#routeTime')
 const form = document.querySelector('#address');
 const searchAddress = document.querySelector('#query');
 const apiKey = '';
@@ -36,9 +56,7 @@ form.addEventListener('submit', async event => {
     event.preventDefault();
     const coordinates = await queryCoordinates();
     const route = await queryRoute(coordinates);
-    console.log(route);
-    //Dekoodaus legGeometry
-    //piirto kartalle reitistä
+    decodePolylineAddToMap(route.data.plan.itineraries[0].legs);
 })
 
 
